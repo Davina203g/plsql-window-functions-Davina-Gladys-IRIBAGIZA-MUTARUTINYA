0b9d7b8ccf02a4870c03428e88cbca3542d535f2
Davina Gladys IRIBAGIZA MUTARUTINYA <72495685+Davina203g@users.noreply.github.com>
2025-09-28 10:58:24 +0200

    Update tables_creation.sql

--- Scripts/tables_creation.sql
+++ Scripts/tables_creation.sql
@@ -27,3 +27,11 @@ CREATE TABLE transactions (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 )
+
+--Insertion of some few data( Two examples per table)
+INSERT INTO transactions (transaction_id, customer_id, product_id, sale_date, quantity, total_amount) VALUES (1021, 1, 107, DATE '2024-07-03', 1, 1500);
+INSERT INTO transactions (transaction_id, customer_id, product_id, sale_date, quantity, total_amount) VALUES (1022, 2, 110, DATE '2024-07-10', 3, 1650);
+INSERT INTO products (product_id, name, category, unit_price) VALUES (109, 'Guava', 'Fruits', 450);
+INSERT INTO products (product_id, name, category, unit_price) VALUES (110, 'Papaya', 'Fruits', 550);
+INSERT INTO customers (customer_id, name, region) VALUES (9, 'Peace', 'Huye');
+INSERT INTO customers (customer_id, name, region) VALUES (8, 'Joseline', 'Muhanga');
