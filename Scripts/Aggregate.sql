1c0255797949c9b93ba22d46d2286d339075c4a9
Davina Gladys IRIBAGIZA MUTARUTINYA <72495685+Davina203g@users.noreply.github.com>
2025-09-28 13:12:20 +0200

    Update Aggregate.sql

--- Scripts/Aggregate.sql
+++ Scripts/Aggregate.sql
@@ -2,13 +2,17 @@ SELECT
     sale_year,
     sale_month,
     monthly_sales,
+    -- RUNNING TOTAL: Tracks performance and overall growth path for each year
     SUM(monthly_sales) OVER (
         PARTITION BY sale_year ORDER BY sale_month ROWS UNBOUNDED PRECEDING
     ) as running_total,
+    --3-MONTH MOVING AVERAGE: Provides trend direction
     AVG(monthly_sales) OVER (
         ORDER BY sale_year, sale_month ROWS 2 PRECEDING
     ) as moving_avg_3month,
+    -- 3-MONTH MAXIMUM: Shows the highest sales in the recent 3 months
     MAX(monthly_sales) OVER (ORDER BY sale_year, sale_month ROWS 2 PRECEDING) as max_3month,
+    -- 3-MONTH MINIMUM: Shows the lowest sales in the recent 3 months
     MIN(monthly_sales) OVER (ORDER BY sale_year, sale_month ROWS 2 PRECEDING) as min_3month
 FROM (
     SELECT
@@ -19,3 +23,6 @@ FROM (
     WHERE sale_date >= DATE '2024-01-01'
     GROUP BY EXTRACT(YEAR FROM sale_date), EXTRACT(MONTH FROM sale_date))
 ORDER BY sale_year, sale_month;
+--INTERPRETATION:
+--The functions help to create an understandable view of sales performance through multiple time-based perspectives,
+--which influences decison making 
